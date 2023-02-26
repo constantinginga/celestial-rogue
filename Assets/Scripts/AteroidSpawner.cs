303ead846f1a1a5e6b23d98ef6fa3c0c92ec6f3e
Nichita Railean <nichita.railean@aol.com>
2023-02-26 19:42:44 +0200

    Asteroid cleanup and stationary

--- Assets/Scripts/AteroidSpawner.cs
+++ Assets/Scripts/AteroidSpawner.cs
@@ -7,18 +7,20 @@ public class AteroidSpawner : MonoBehaviour
 {
     [SerializeField] private AsteroidScript asteroidPrefab;
     [SerializeField] private float spawnRate = 2.0f;
-    [SerializeField] private int spawnAmount = 1;
+    [SerializeField] private int spawnAmountMoving = 1;
+    [SerializeField] private int spawnAmountStationary = 10;
     [SerializeField] private float spawnDistance = 50.0f;
     [SerializeField] private float trajectoryVariance = 15.0f;
 
     void Start()
     {
         InvokeRepeating(nameof(spawn), this.spawnRate, this.spawnRate );
+        spawnStationary();
     }
 
     private void spawn()
     {
-        for (int i = 0; i < this.spawnAmount; i++)
+        for (int i = 0; i < this.spawnAmountMoving; i++)
         {
             Vector3 spawnDirection = Random.insideUnitCircle.normalized * spawnDistance;
             Vector3 spawnPoint = this.transform.position + spawnDirection;
@@ -32,5 +34,16 @@ public class AteroidSpawner : MonoBehaviour
            asteroid.setTrajectory(rotation * -spawnDirection);
         }
     }
-   
+
+    private void spawnStationary()
+    {
+        for (int i = 0; i < spawnAmountStationary; i++)
+        {
+            Vector3 spawnPoint = new Vector3(Random.Range(-50, 50),Random.Range(-20, 20),0);
+
+            AsteroidScript asteroid = Instantiate(this.asteroidPrefab, spawnPoint, this.transform.rotation);
+            asteroid.size = asteroid.maxSize;
+        }
+    }
+
 }
