45e18d597935397046a37bf4c8c19f3500af6e05
Constantin Ginga <costelgn@gmail.com>
2023-03-03 14:27:55 +0100

    Connect healthbar to player hp & Update health bar using delegates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Slider healthBar;
     private Rigidbody2D rb;
     private InputController input;
+    public delegate void TakeDamageDelegate(int damageAmount);
+    public event TakeDamageDelegate TakeDamageEvent;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         input = GetComponent<InputController>();
+        currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
-    void FixedUpdate() 
+    void FixedUpdate()
     {
-        //rb.velocity = new Vector2(input.movementPos.x, input.movementPos.y) * speed;
         rb.AddRelativeForce(new Vector2(input.movementPos.x, input.movementPos.y) * speed);
-        
+    }
+
+    // THIS IS ONLY FOR TESTING, REMOVE THIS LATER WHEN THE ENEMY CAN DAMAGE THE PLAYER
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         TakeDamage(10);
+    //     }
+    // }
+
+    private void TakeDamage(int damageAmount)
+    {
+        if (currentHealth <= 0)
+        {
+            Die();
+            return;
+        }
+        currentHealth -= damageAmount;
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.value = currentHealth;
+    }
+
+    private void Die()
+    {
+        // Handle death
+    }
+
+    private void OnEnable()
+    {
+        TakeDamageEvent += TakeDamage;
+    }
+
+    private void OnDisable()
+    {
+        TakeDamageEvent -= TakeDamage;
     }
 }
