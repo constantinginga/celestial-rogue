858d54c6bdc7024eddaceeca315a0244f9e1d2bd
Jakub_Platzek <jakub.platzek@gmail.com>
2023-03-22 11:14:41 +0100

    Small fixes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -54,7 +54,7 @@ public class EnemyController : MonoBehaviour
 
 			break;
 		case SpaceshipsEnum.Enemy_Grey:
-
+			gameObject.AddComponent<EnemyGrey>();
 			break;
 		case SpaceshipsEnum.Enemy_Purple:
 			gameObject.AddComponent<PurpleEnemyController>();
